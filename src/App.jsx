009073d39a37dab6c5471ea08d5d5bc63c408555
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-10-29 22:27:04 +0900

    added background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Hi, I'm Afiq.</h1>
-      <h3>A web developer and self-proclaimed photographer.</h3>
+      <h2>A web developer and self-proclaimed photographer.</h2>
     </>
   )
 }
