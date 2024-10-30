a9af7a2c47b905b3922a6ac3a20a141498fa2185
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-10-31 00:12:15 +0900

    added about section

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <div className='about'>
+            <h1>Yellow, this is the about page</h1>
+        </div>
+    )
+}
+
+export default About
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './Navbar.jsx'
+import About from './About.jsx'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <h1>Hi, I'm Afiq.</h1>
         <h2>A web developer and self-proclaimed photographer.</h2>
       </div>
+      <About />
     </>
   )
 }
