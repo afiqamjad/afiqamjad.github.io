4dcdbe6e870fb26d59152337bff8690687bc5843
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-10-30 21:11:34 +0900

    Added simple navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Navbar from './Navbar.jsx'
 
 function App() {
 
   return (
     <>
-      <h1>Hi, I'm Afiq.</h1>
-      <h2>A web developer and self-proclaimed photographer.</h2>
+      <Navbar />
+      <div className='intro'>
+        <h1>Hi, I'm Afiq.</h1>
+        <h2>A web developer and self-proclaimed photographer.</h2>
+      </div>
     </>
   )
 }
