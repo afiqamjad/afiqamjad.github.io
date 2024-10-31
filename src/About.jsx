f672b895a5009bb11328e99725a55233596413ee
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-10-31 15:27:17 +0900

    Added about me section

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,9 @@ import './About.css'
 const About = () => {
     return (
         <div className='about'>
-            <h1>Yellow, this is the about page</h1>
+            <h2 className='aboutMe'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere pretium nisl eget iaculis. Fusce ac porta tellus, sit amet mattis urna. In id rutrum lectus. Aliquam consequat neque aliquam dignissim dapibus. Vestibulum lacus urna, laoreet ut erat quis, volutpat sodales orci. Etiam ex nunc, malesuada et eros ut, elementum rutrum leo. Suspendisse tincidunt at arcu sed dictum. Integer suscipit nulla vel facilisis iaculis. Praesent laoreet non libero in mattis. Sed a arcu sit amet diam laoreet bibendum vulputate vitae sem. Cras sodales justo tortor, nec dictum nibh pretium a. Nulla porttitor mauris orci, quis commodo nulla maximus at. Donec ultrices libero sed justo lobortis efficitur. Vivamus feugiat dolor quis odio auctor imperdiet. Aliquam blandit sagittis lectus nec porta. Quisque ligula odio, vestibulum a ex ac, finibus accumsan odio</h2>
+            <div className='profpic'>
+            </div>
         </div>
     )
 }
